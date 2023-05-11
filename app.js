07fb8b54914798d7ebb0adf413879a705e4b024b
88joonyc <joonyc88@gmail.com>
2023-05-11 00:02:24 -0400

    cors

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (!isProduction) {
-    app.use(cors());
+    app.use(cors(
+      {
+        origin:'*', 
+        credentials:true,            //access-control-allow-credentials:true
+        optionSuccessStatus:200,
+      }
+    ));
 }
 app.use(helmet({
     contentSecurityPolicy: false
