79c4e8e4cc39433aa13eb08a33e78442442a3ddf
88joonyc <joonyc88@gmail.com>
2023-05-12 00:37:45 -0400

    v2/reference/news

--- routes/api/allStocks.js
+++ routes/api/allStocks.js
@@ -9,11 +9,10 @@ const jsonParser = bodyParser.json();
 // app.use(cors());
 
 const headerOptions = {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${process.env.APISECRETEKEY}`,
-    
-    }
+  method: 'GET',
+  headers: {
+    'Authorization': `Bearer ${process.env.APISECRETEKEY}`,
+  }
 }
 
 // get all tickers
@@ -33,7 +32,7 @@ router.get('/details/:ticker', (req, res) => {
 
 router.get('/news/:ticker', async (req, res) => {
   try {
-    const news = await fetch(`${process.env.APIENDPOINT}/news?ticker=${req.params.ticker.toUpperCase()}`, headerOptions)
+    const news = await fetch(`${process.env.APIENDPOINT}v2/reference/news?ticker=${req.params.ticker.toUpperCase()}`, headerOptions)
     if (news.ok) {
       const json = await news.json()
       console.log(json)
