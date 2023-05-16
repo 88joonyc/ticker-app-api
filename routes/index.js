34fddda3e19703817e2d10134af3570b470d1631
88joonyc <joonyc88@gmail.com>
2023-05-16 00:09:51 -0400

    home

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 if (process.env.NODE_ENV === 'production') {
   
-  router.get('/api/csrf/restore', (req, res) => {
+  router.get('/', (req, res) => {
     res.cookie('XSRF-TOKEN', req.csrfToken(), {
       maxAge: 604800 * 1000, 
       httpOnly: false,
