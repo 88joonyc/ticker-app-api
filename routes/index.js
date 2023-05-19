151394bbf82c7a09945ac604922d34521ce5d94a
88joonyc <joonyc88@gmail.com>
2023-05-19 18:31:01 -0400

    index.js

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,20 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
     const path = require('path');
     // Serve the frontend's index.html file at the root route
     router.get('/', (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
-      return res.sendFile(
-        path.resolve(__dirname, 'client/build', 'build', 'index.html')
-      );
+      console.log('fiondme-------------------------------------------', req.csrfToken())
+      return res.cookie('XSRF-TOKEN', req.csrfToken(),  {
+        maxAge: 604800 * 1000, 
+        httpOnly: false,
+        secure: true,
+        sameSite: 'none',
+      });
+      // return res.cookie(
+      //   path.resolve(__dirname, 'client/build', 'build', 'index.html')
+      // );
     });
   
     // Serve the static assets in the frontend's build folder
@@ -20,16 +26,24 @@ if (process.env.NODE_ENV !== 'production') {
   
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
-      return res.sendFile(
-        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
-      );
+      console.log('fiondme2-------------------------------------------', req.csrfToken())
+      return res.cookie('XSRF-TOKEN', req.csrfToken(),  {
+        maxAge: 604800 * 1000, 
+        httpOnly: false,
+        secure: true,
+        sameSite: 'none',
+      });
+      // return res.sendFile(
+      //   path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      //   );
     });
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV !== 'production') {
   router.get('/api/csrf/restore', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken(), {
+
+
+      res.cookie('XSRF-TOKEN', req.csrfToken(), {
       maxAge: 604800 * 1000, 
       httpOnly: false,
       secure: true,
